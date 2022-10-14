5b249f53acfcbe9f482c805586a10b301b27ac06
serge.ayer <42145027+SergeAyer@users.noreply.github.com>
2022-10-14 12:01:03 +0200

    Added clock with mutex

---
+++ clock_with_mutex.hpp
@@ -0,0 +1,88 @@
+#pragma once
+
+
+#include <chrono>
+class Clock {
+public:
+    struct DateTimeType {
+        uint32_t day;
+        uint32_t hour;
+        uint32_t minute;
+        uint32_t second;
+    };
+
+    Clock()
+    {
+        // initialize current time
+        _currentTime.day = 0;
+        _currentTime.hour = 10;
+        _currentTime.minute = 59;
+        _currentTime.second = 59;
+    }
+
+    void start()
+    {
+        // start a ticker thread for dispatching events that are queued in the tickerUpdate() method
+        _tickerThread.start(callback(&_tickerQueue, &EventQueue::dispatch_forever));
+        // call the tickerUpdate() method every second, for queueing an event to be dispatched by the ticker thread
+        _ticker.attach(callback(this, &Clock::tickerUpdate), clockUpdateTimeout);
+
+        // schedule an event every second for displaying the time on the console
+        _clockDisplayQueue.call_every(clockDisplayTimeout, callback(this, &Clock::getAndPrintDateTime));
+        // dispatch events from the thread calling the start() method (main thread)
+        _clockDisplayQueue.dispatch_forever();
+    }
+
+private:
+    void getAndPrintDateTime()
+    {
+        DateTimeType dt = {0};
+
+        _currentTimeMutex.lock();
+
+        dt.day = _currentTime.day;
+        dt.hour = _currentTime.hour;
+        dt.minute = _currentTime.minute;
+        dt.second = _currentTime.second;
+
+        _currentTimeMutex.unlock();
+
+        printf("Day %d Hour %d min %d sec %d\n", dt.day, dt.hour, dt.minute, dt.second);
+    }
+
+    void tickerUpdate()
+    {
+        _tickerQueue.call(callback(this, &Clock::updateCurrentTime));
+    }
+
+    void updateCurrentTime()
+    {
+        _currentTimeMutex.lock();
+
+        _currentTime.second += 
+          std::chrono::duration_cast<std::chrono::seconds>(clockUpdateTimeout).count();
+        if (_currentTime.second > 59) {
+            _currentTime.second = 0;
+            _currentTime.minute++;
+            if (_currentTime.minute > 59) {
+                _currentTime.minute = 0;
+                _currentTime.hour++;
+                if (_currentTime.hour > 23) {
+                    _currentTime.hour = 0;
+                    _currentTime.day++;
+                }
+            }
+        }
+
+        _currentTimeMutex.unlock();
+    }
+
+    EventQueue _clockDisplayQueue;
+    Ticker _ticker;
+    EventQueue _tickerQueue;
+    Thread _tickerThread;
+    DateTimeType _currentTime;
+    Mutex _currentTimeMutex;
+    static constexpr std::chrono::milliseconds clockUpdateTimeout = 1000ms;
+    static constexpr std::chrono::milliseconds clockDisplayTimeout = 1000ms;
+};
